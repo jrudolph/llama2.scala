c51017f8df547ff585dbae1926afa0f5ebdfd1f3
Johannes Rudolph <johannes.rudolph@gmail.com>
2023-08-10 16:57:33 +0200

    finally support prompts

--- shared/src/main/scala/net/virtualvoid/llama2/Llama2Runner.scala
+++ shared/src/main/scala/net/virtualvoid/llama2/Llama2Runner.scala
@@ -3,7 +3,8 @@ package net.virtualvoid.llama2
 class Llama2Runner(transformer: Llama2Transformer, model: Llama2Model) {
   import model.vocab
 
-  def iterate(steps: Int): Iterator[String] = new Iterator[String] {
+  def iterate(steps: Int, prompt: String = ""): Iterator[String] = new Iterator[String] {
+    val promptTokens = bpeEncode(prompt)
 
     var pos = 0
     var token = 1
@@ -11,12 +12,37 @@ class Llama2Runner(transformer: Llama2Transformer, model: Llama2Model) {
     def hasNext: Boolean = pos < steps && token != 0
     def next(): String = {
       val logits = transformer.step(token, pos)
-      val next = logits.zipWithIndex.maxBy(_._1)._2 // argmax
+      val next =
+        if (pos < promptTokens.length) promptTokens(pos)
+        else
+          logits.zipWithIndex.maxBy(_._1)._2 // argmax
       val tok = vocab.tokenScores(next)._1
       val tokenStr = if (token == 1 && tok == " ") tok.drop(1) else tok
       token = next
       pos += 1
       tokenStr
     }
+  }
+
+  def bpeEncode(prompt: String): Seq[Int] = {
+    val tokMap = vocab.tokenScores.zipWithIndex.map { case ((t, s), i) => t -> (i, s) }.toMap
+    var toks: Vector[Int] = prompt.map(x => tokMap(x.toString)._1).toVector
+
+    while (true) {
+      val candidates =
+        toks.sliding(2).zipWithIndex.flatMap {
+          case (Seq(t1: Int, t2: Int), i: Int) =>
+            val tok = vocab.tokenScores(t1)._1 + vocab.tokenScores(t2)._1
+            val id = tokMap.get(tok)
+            id.map { case (newTok, score) => (i, newTok, score) }
+        }
+
+      if (candidates.isEmpty) return toks
+      else {
+        val (idx, newTok, _) = candidates.maxBy(_._3)
+        toks = toks.take(idx) ++ Seq(newTok) ++ toks.drop(idx + 2)
+      }
+    }
+    throw new IllegalStateException("Should not be reachable")
   }
 }
