a67c448e8d395bfd9f2d94b45c85ea12ceea1aa5
Johannes Rudolph <johannes.rudolph@gmail.com>
2023-08-31 10:24:47 +0200

    provide model name from file name

--- shared/src/main/scala/net/virtualvoid/llama2/Llama2Model.scala
+++ shared/src/main/scala/net/virtualvoid/llama2/Llama2Model.scala
@@ -3,16 +3,19 @@ package net.virtualvoid.llama2
 import java.io.File
 
 trait Llama2Model { outer =>
+  def name: String
   val config: Config
   val vocab: Vocab
   val weights: Weights
 
   def quantizeQ4: Llama2Model = new Llama2Model {
+    val name = outer.name + "_q4"
     val config: Config = outer.config
     val vocab: Vocab = outer.vocab
     val weights: Weights = outer.weights.quantizeQ4
   }
   def quantizeQ8: Llama2Model = new Llama2Model {
+    val name = outer.name + "_q8"
     val config: Config = outer.config
     val vocab: Vocab = outer.vocab
     val weights: Weights = outer.weights.quantizeQ8
@@ -21,10 +24,12 @@ trait Llama2Model { outer =>
 object Llama2Model {
   def fromGgml(ggmlFile: File): Llama2Model =
     new Llama2Model {
+      val name = ggmlFile.getName.reverse.dropWhile(_ != '.').drop(1).reverse
       val (config, vocab, weights) = GgmlLoader.fromGgml(ggmlFile)
     }
   def fromLlama2CModel(checkpointFile: File, tokenizerFile: File): Llama2Model =
     new Llama2Model {
+      val name = checkpointFile.getName.reverse.dropWhile(_ != '.').drop(1).reverse
       val config = Config.fromFile(checkpointFile)
       val vocab = Vocab.fromFile(config, tokenizerFile)
       val weights = Weights.fromFile(config, checkpointFile)
